20bb88734c65b11a5111c31efe5ff6b163f5d7aa
Bruce439999-gif <bruce439999@gmail.com>
2025-12-26 18:37:17 +0800

    更新 Giscus 配置 ID

--- src/components/Giscus.tsx
+++ src/components/Giscus.tsx
@@ -11,14 +11,14 @@ export default function GiscusComments() {
       <Giscus
         id="comments"
         repo="bruce439999-gif/app2"
-        repoId="R_kgDONn5R4w" // 这通常是默认 ID，如果不对请告诉我
-        category="General"
-        categoryId="DIC_kwDONn5R484Cl646" // 这通常是默认 ID，如果不对请告诉我
+        repoId="R_kgDOQvCZEQ"
+        category="Announcements"
+        categoryId="DIC_kwDOQvCZEc4C0QU1"
         mapping="pathname"
         term="Welcome to my blog!"
         reactionsEnabled="1"
         emitMetadata="0"
-        inputPosition="top"
+        inputPosition="bottom"
         theme="preferred_color_scheme"
         lang="zh-CN"
         loading="lazy"
